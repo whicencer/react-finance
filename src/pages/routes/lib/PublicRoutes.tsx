41e9684cc3b2e52321807bc6d76ab7d7a15bb57f
whicencer <denielaz2020@gmail.com>
2022-07-03 23:00:16 +0300

    refactor: change static isAuth to hook useAuth

--- src/pages/routes/lib/PublicRoutes.tsx
+++ src/pages/routes/lib/PublicRoutes.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
+import { useAuth } from '../../../app/hooks/useAuth';
 
 const PublicRoutes = () => {
-  const isAuth = false;
+  const { isAuth } = useAuth();
 
   return !isAuth ? <Outlet /> : <Navigate to={'/dashboard'} />;
 };
