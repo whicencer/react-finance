a0a5701ba6b746fb8b608735a316e76f03606778
whicencer <denielaz2020@gmail.com>
2022-10-11 21:10:53 +0300

    refactor: Got rid of code repetition in styles

---
+++ src/styles/pageContent.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const PageContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  max-width: 960px;
+  margin: 0 auto;
+`;
