1a13c88464f14fc805d54932b1ca09fb282ed8fe
whicencer <denielaz2020@gmail.com>
2022-11-05 22:45:16 +0200

    refactor: changed card style

--- src/components/CreditCard/creditCard.styles.ts
+++ src/components/CreditCard/creditCard.styles.ts
@@ -15,6 +15,7 @@ export const Card = styled.div<{themeId: number}>`
   background-size: cover;
   border-radius: 20px;
   padding: 10px 25px;
+  margin: 0 10px;
   max-width: 225px;
   height: 150px;
   min-width: 250px;
