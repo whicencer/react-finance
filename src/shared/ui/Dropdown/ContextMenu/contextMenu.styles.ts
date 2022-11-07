1be4f1f422a1602d688dd6588a960d48feafd517
whicencer <denielaz2020@gmail.com>
2022-11-07 22:54:20 +0200

    fix: start to fixing problem with context menu

--- src/shared/ui/Dropdown/ContextMenu/contextMenu.styles.ts
+++ src/shared/ui/Dropdown/ContextMenu/contextMenu.styles.ts
@@ -3,5 +3,5 @@ import { DropdownStyles } from '../dropdown.styles';
 
 export const ContextMenuStyles = styled(DropdownStyles)<{x: number, y: number}>`
   top: ${props => props.y}px;
-  left: calc(${props => props.x}px - 105px);
+  left: ${props => props.x+200 >= window.innerWidth ? `calc(${window.innerWidth}-200px)` : `${props.x}px`};
 `;
