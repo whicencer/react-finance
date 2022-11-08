98b794167cf10437cbb5b94c2c717f97f4ef4873
whicencer <denielaz2020@gmail.com>
2022-11-08 21:24:05 +0200

    remove console.log

--- src/shared/ui/Dropdown/ContextMenu/contextMenu.styles.ts
+++ src/shared/ui/Dropdown/ContextMenu/contextMenu.styles.ts
@@ -4,7 +4,6 @@ import { DropdownStyles } from '../dropdown.styles';
 export const ContextMenuStyles = styled(DropdownStyles)<{x: number, y: number}>`
   top: ${props => props.y}px;
   left: ${props => {
-    console.log(props.x);
     if (props.x+200 > window.innerWidth) {
       return `calc(${window.innerWidth}-200px)`;
     } else if (props.x <= 0) {
