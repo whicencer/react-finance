04cfba9312fae58feff5eb08399e22dee1393e30
whicencer <denielaz2020@gmail.com>
2022-11-08 21:23:48 +0200

    fix: fixed ContextMenu which that pops out of display

--- src/shared/ui/Dropdown/ContextMenu/contextMenu.styles.ts
+++ src/shared/ui/Dropdown/ContextMenu/contextMenu.styles.ts
@@ -3,5 +3,14 @@ import { DropdownStyles } from '../dropdown.styles';
 
 export const ContextMenuStyles = styled(DropdownStyles)<{x: number, y: number}>`
   top: ${props => props.y}px;
-  left: ${props => props.x+200 >= window.innerWidth ? `calc(${window.innerWidth}-200px)` : `${props.x}px`};
+  left: ${props => {
+    console.log(props.x);
+    if (props.x+200 > window.innerWidth) {
+      return `calc(${window.innerWidth}-200px)`;
+    } else if (props.x <= 0) {
+      return `${props.x - props.x + 20}px`;
+    } else {
+      return `${props.x}px`;
+    }
+  }};
 `;
