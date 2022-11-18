3358be8d8c04d43707c574aafeff2a1bf547a3d8
whicencer <denielaz2020@gmail.com>
2022-11-18 18:37:02 +0200

    changing document title at main page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Routing from "../pages/routes";
+import { useDocumentTitle } from "./hooks/useDocumentTitle";
 import { withProviders } from "./providers";
 
 const App = () => {
+  useDocumentTitle('React Finance - Welcome');
   return (
     <Routing />
   );
