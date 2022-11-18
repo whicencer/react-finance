426e0ed8edd85602bf7eeda37106afc2036aefb2
whicencer <denielaz2020@gmail.com>
2022-11-18 14:36:19 +0200

    refactor: changed button style in DatePicker

--- src/shared/ui/DatePicker/index.tsx
+++ src/shared/ui/DatePicker/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useAwayClick } from '../../../app/hooks/useAwayClick';
-import Button from '../Button';
 import Flex from '../Flex';
 import styles from './DatePicker.module.scss';
 
@@ -23,23 +22,21 @@ export const DatePicker: React.FC<IDatePickerProps> = ({ onChange }) => {
 
   return (
     <Flex onClick={(e) => e.stopPropagation()} className={styles.datePickerWrapper}>
-      <Flex direction='column' onClick={() => setChooseDateActive(!chooseDateActive)} className={styles.datePickerButton}>
-        <Flex justifyContent='space-between' alignItems='center'>
-          <p>Choose date</p>
-          <p style={{ marginLeft: 5 }}>&#128197;</p>
-        </Flex>
-        <p>{monthsText[currentMonth]} {currentYear}</p>
+      <Flex justifyContent='space-between' alignItems='center' onClick={() => setChooseDateActive(!chooseDateActive)} className={styles.datePickerButton}>
+        <p>Choose date</p>
+        <p style={{ marginLeft: 5 }}>&#128197;</p>
       </Flex>
       <Flex direction='column' alignItems='center' className={chooseDateActive ? styles.datePicker+" "+styles.active : styles.datePicker}>
         <Flex style={{ width: '90%' }} justifyContent='space-between' alignItems='center'>
-          <Button>{'<'}</Button>
+          <button>{'<'}</button>
           <span>{currentYear}</span>
-          <Button>{'>'}</Button>
+          <button>{'>'}</button>
         </Flex>
         <Flex style={{ width: '90%', marginTop: '15px' }} wrap={'wrap'} justifyContent='space-between'>
           {
             monthsText.map(month => {
-              return <Flex onClick={() => setCurrentMonth(new Date(`2022-${month}-1`).getMonth())} justifyContent='center' alignItems='center' className={styles.datePickerMonth} key={month}>{month}</Flex>;
+              const monthToNumber = new Date(`2022-${month}-1`).getMonth();
+              return <Flex onClick={() => setCurrentMonth(monthToNumber)} justifyContent='center' alignItems='center' className={monthToNumber === currentMonth ? styles.datePickerMonth+" "+styles.active : styles.datePickerMonth} key={month}>{month}</Flex>;
             })
           }
         </Flex>
