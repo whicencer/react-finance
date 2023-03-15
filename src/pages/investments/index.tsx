49a501a56d9d49f6a37f6325040ea0f8ac5df9d2
Anna Alyoshina <denielaz2020@gmail.com>
2023-03-15 18:12:18 +0200

    refactor: remove Create Portfolio button

--- src/pages/investments/index.tsx
+++ src/pages/investments/index.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { toast } from 'react-toastify';
 import { useDocumentTitle } from '../../app/hooks/useDocumentTitle';
-import Flex from '../../shared/ui/Flex';
-import { PageButton } from '../../shared/ui/PageButton';
 import { CryptoChange24 } from './components/CryptoChange24';
 import { CryptoSearch } from './components/CryptoSearch';
 
@@ -10,14 +7,11 @@ const Investments = () => {
   useDocumentTitle('React Finance - Investments');
   
   return (
-    <div>
-      <Flex justifyContent='space-between'>
-        <h2>Investments</h2>
-        <PageButton onClick={() => toast.info(`😒 I'm still in development`)}>Create portfolio</PageButton>
-      </Flex>
+    <>
+      <h2>Investments</h2>
       <CryptoChange24 />
       <CryptoSearch />
-    </div>
+    </>
   );
 };
 
