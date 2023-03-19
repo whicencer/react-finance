9fac61c1cc6fe24115efaa0e32c9e5f76ab914b4
Anna Alyoshina <denielaz2020@gmail.com>
2023-03-19 02:44:16 +0200

    vite.config.ts: outDir in build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build'
+  },
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
   resolve: {
     alias: [
