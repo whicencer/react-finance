fbe353f35632fd165c0166070815c3eb4ebf17fe
Anna Alyoshina <denielaz2020@gmail.com>
2023-04-22 20:52:41 +0300

    reducers

--- src/store/slices/creditCards/creditCards.reducers/cards/deleteCard.ts
+++ src/store/slices/creditCards/creditCards.reducers/cards/deleteCard.ts
@@ -4,7 +4,7 @@ import { ICardsState } from "../../creditCards.typings";
 export const deleteCardReducer = (state: ICardsState, action: PayloadAction<string>) => {
 
   const idInState = state.cards.items.findIndex(el => el.card_id === action.payload);
-  const updateTransactions = state.transactions.items.filter(transaction => transaction.balance !== action.payload);
+  const updateTransactions = state.transactions.items.filter(transaction => transaction.balanceId !== action.payload);
 
   state.cards.items.splice(idInState, 1);
   state.transactions.items = updateTransactions;
